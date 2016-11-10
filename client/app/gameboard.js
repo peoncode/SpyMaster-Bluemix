1b9406408ec16811e559076eccc85fbccbd9ca2c
peoncode <peoncode@users.noreply.github.com>
2016-11-10 01:09:08 -0800

    Updated front-end code to call backend for gamedata

--- client/app/gameboard.js
+++ client/app/gameboard.js
@@ -2,18 +2,28 @@
 (function() {
 
 function GameBoardCtrl($scope, gameDataService) {
-  $scope.gameId = gameDataService.getData()["gameId"];
-  $scope.gameRound = gameDataService.getData()["gameRound"];
-  $scope.blueScore = gameDataService.getData()["blueScore"];
-  $scope.redScore = gameDataService.getData()["redScore"];
-  $scope.whoStarts = gameDataService.getData()["whoStarts"];
-  $scope.cardList = JSON.parse(JSON.stringify(gameDataService.getData()["cardList"]));
-  $scope.blueCardsOpen = 0;
-  $scope.redCardsOpen = 0;
-  $scope.blueWins = false;
-  $scope.redWins = false;
-  $scope.whosTurn = $scope.whoStarts;
 
+
+  
+  gameDataService.getData(function (err, gameData) {
+    if (err) {
+      $scope.page_load_error = "Unexpected error loading game data: " + err.message;
+      return;
+    }
+    else {
+      $scope.gameId = gameData["gameId"];
+      $scope.whoStarts = gameData["whoStarts"];
+      $scope.cardList = JSON.parse(JSON.stringify(gameData["cardList"]));
+      $scope.gameRound = 1;
+      $scope.blueScore = 0;
+      $scope.redScore = 0;
+      $scope.blueCardsOpen = 0;
+      $scope.redCardsOpen = 0;
+      $scope.blueWins = false;
+      $scope.redWins = false;
+      $scope.whosTurn = $scope.whoStarts;
+    }
+  });  
    
   $scope.revealCard = function(index) {
     var card = $scope.cardList[index];
@@ -78,13 +88,11 @@ function GameBoardCtrl($scope, gameDataService) {
   function blueWins() {
     $scope.blueWins = true;
     $scope.blueScore++;
-    gameDataService.set("blueScore", $scope.blueScore);
   }
 
   function redWins() {
     $scope.redWins = true;
     $scope.redScore++;
-    gameDataService.set("redScore", $scope.redScore);
   }
 
   function gameOver() {
@@ -100,21 +108,45 @@ function GameBoardCtrl($scope, gameDataService) {
       console.log("Game over. RED wins!");
     }
 
-    gameDataService.set("gameRound", $scope.gameRound+1);
+    $scope.gameRound++;
   }
 
   $scope.newRound = function() {
     console.log("Preparing New round");
-    $scope.gameRound = gameDataService.getData()["gameRound"];
-    $scope.whoStarts = gameDataService.getData()["whoStarts"];
-    $scope.redScore = gameDataService.getData()["redScore"];
-    $scope.blueScore = gameDataService.getData()["blueScore"];
-    $scope.cardList = JSON.parse(JSON.stringify(gameDataService.getData()["cardList"]));
-    $scope.blueWins = false;
-    $scope.redWins = false;
-    $scope.redCardsOpen = 0;
-    $scope.blueCardsOpen = 0;
-    $scope.whosTurn = $scope.whoStarts;
+    gameDataService.getData(function (err, gameData) {
+      if (err) {
+        $scope.page_load_error = "Unexpected error loading game data: " + err.message;
+        return;
+      }
+      else {
+        // $scope.gameRound = gameData["gameRound"];
+        // $scope.blueScore = gameData["blueScore"];
+        // $scope.redScore = gameData["redScore"];
+        $scope.gameId = gameData["gameId"];
+        $scope.whoStarts = gameData["whoStarts"];
+        $scope.cardList = JSON.parse(JSON.stringify(gameData["cardList"]));
+        $scope.blueCardsOpen = 0;
+        $scope.redCardsOpen = 0;
+        $scope.blueWins = false;
+        $scope.redWins = false;
+        $scope.whosTurn = $scope.whoStarts;
+      }
+    });  
+
+
+
+
+    // $scope.gameRound = gameDataService.getData()["gameRound"];
+    //$scope.gameId = gameData["gameId"];
+    //$scope.whoStarts = gameDataService.getData()["whoStarts"];
+    // $scope.redScore = gameDataService.getData()["redScore"];
+    // $scope.blueScore = gameDataService.getData()["blueScore"];
+    //$scope.cardList = JSON.parse(JSON.stringify(gameDataService.getData()["cardList"]));
+    // $scope.blueWins = false;
+    // $scope.redWins = false;
+    // $scope.redCardsOpen = 0;
+    // $scope.blueCardsOpen = 0;
+    // $scope.whosTurn = $scope.whoStarts;
 
     console.log("after: Starting ROUND " + $scope.gameRound);
   }
